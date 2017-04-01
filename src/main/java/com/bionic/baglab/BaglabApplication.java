82002de21a9c4eb0d3c7068712a195ae152bdaa1
Anton <anton.pasichnyk@gmail.com>
2017-04-01 18:50:52 +0300

    fix BaglabApplication

--- src/main/java/com/bionic/baglab/BaglabApplication.java
+++ src/main/java/com/bionic/baglab/BaglabApplication.java
@@ -2,15 +2,12 @@ package com.bionic.baglab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.bionic.baglab")
-@EntityScan(basePackages = {"com.bionic.baglab.domains", "com.bionic.baglab.controllers"})
 public class BaglabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaglabApplication.class, args);
 	}
+
 }
